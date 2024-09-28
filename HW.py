baf71ec0d5483b47bf616ceaa8dbc8094f2d9dff
stefania <stefaniablyumkina@yandex.ru>
2024-09-28 15:16:22 +0300

    c:\Users\79161\Lab-1\HW.py

---
+++ HW.py
@@ -0,0 +1,29 @@
+#1
+'''print("\x1b[48;5;1;1m        \x1b[0m")
+print("\x1b[48;5;7;1m        \x1b[0m")
+print("\x1b[48;5;4;1m        \x1b[0m")
+print('\x1b[0m')'''
+#2
+'''a = b = c = d = e = ""
+while True:
+    a += "\x1b[48;5;4;1m   \x1b[0m         \x1b[48;5;4;1m   \x1b[0m         \x1b[48;5;4;1m   \x1b[0m"
+    b += "   \x1b[48;5;4;1m   \x1b[0m   \x1b[48;5;4;1m   \x1b[0m   \x1b[48;5;4;1m   \x1b[0m   \x1b[48;5;4;1m   \x1b[0m   "
+    c += "      \x1b[48;5;4;1m   \x1b[0m         \x1b[48;5;4;1m   \x1b[0m      "
+    d += "   \x1b[48;5;4;1m   \x1b[0m   \x1b[48;5;4;1m   \x1b[0m   \x1b[48;5;4;1m   \x1b[0m   \x1b[48;5;4;1m   \x1b[0m   "
+    e += "\x1b[48;5;4;1m   \x1b[0m         \x1b[48;5;4;1m   \x1b[0m         \x1b[48;5;4;1m   \x1b[0m"
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)'''
+#3
+'''print("            \x1b[48;5;7;1m   \x1b[0m")
+print("            \x1b[48;5;7;1m   \x1b[0m")
+print("         \x1b[48;5;7;1m   \x1b[0m")
+print("         \x1b[48;5;7;1m   \x1b[0m")
+print("      \x1b[48;5;7;1m   \x1b[0m")
+print("      \x1b[48;5;7;1m   \x1b[0m")
+print("   \x1b[48;5;7;1m   \x1b[0m")
+print("   \x1b[48;5;7;1m   \x1b[0m")
+print("\x1b[48;5;7;1m   \x1b[0m")
+print("\x1b[48;5;7;1m   \x1b[0m")'''
